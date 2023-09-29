d4d284c0a41dbdcf1914bc660243187b0ffdf1c2
isaac <isaach2026@student.cis.edu.hk>
2023-09-29 12:33:39 +0800

    Lab 1 done

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age");
+                int age = scanner.nextInt();
+                int dateable = ( 7 + ( age / 2 ) );
+        System.out.println(age + " year olds can date " + dateable + " year olds and above");
     }
 }
